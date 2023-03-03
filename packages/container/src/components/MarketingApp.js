c24ac32a9a75a092bcc1892c59df999cf2d3f8e0
Pavel Didenko <paveldidenko@Pavels-Laptop.local>
2023-03-03 15:40:36 +0700

    initial commit

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef } from 'react';
+import { mount } from 'marketing/MarketingApp';
+
+export const MarketingApp = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return (
+    <div ref={ref} />
+  )
+};
